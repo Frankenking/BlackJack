1c564152dc2ee2edac217f637a777757f8a055f8
Frankenking <99056962+Frankenking@users.noreply.github.com>
2022-10-21 06:22:13 -0700

    e

--- BlackJack.py
+++ BlackJack.py
@@ -1,7 +1,17 @@
 import os
-from re import S 
 import time
 import ascii_assets as ascii
+import random
+
+def clear():
+    os.system('cls')
+    
+def hit():
+    card = deck[random.randint(0,len(deck)-1)]
+    card_remove = deck.index(card)
+    deck.pop(card_remove)
+    return(card)
+
 
 def shuffle_deck():
     global deck, evaluation
@@ -18,14 +28,14 @@ def shuffle_deck():
     for suit in suits:
         for card in cards:
             deck.append(card + ' of ' + suit)
-            values.extend([1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 10, 10, 10, 11])
+    for suit in suits:        
+        values.extend([1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 10, 10, 10, 11])
             
     card_evaluation = dict(zip(deck, values))
-    print(card_evaluation)
 
-    return deck
+
 def __init__():
     shuffle_deck()
-
+    
 if __name__ == "__main__":
     __init__()
