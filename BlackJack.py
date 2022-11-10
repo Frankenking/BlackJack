3ffa84c580845494a62e76121a97b7fa5517e351
Frankenking <99056962+Frankenking@users.noreply.github.com>
2022-11-10 00:39:12 -0700

    Find Very Rare Bug thing fix everything else works playtest more

--- BlackJack.py
+++ BlackJack.py
@@ -6,7 +6,7 @@
 ###############################
 
 
-
+#MODULES
 import os
 import time
 import ascii_assets as ascii
@@ -32,14 +32,13 @@ def hit():
     deck.pop(card_index)
     return(card)
 
-        #this function shuffles the deck by making a dictionary and an array containg all cards in a full deck
+        #this function shuffles the deck by making an array containg all cards in a full deck
 def shuffle_deck():
-    global deck, evaluation
+    global deck
     
     deck = []
     
     values = []
-    evaluation = []
     
     suits = ["Spades", "Diamonds", "Clubs", "Hearts"]
     
@@ -52,280 +51,208 @@ def shuffle_deck():
     for suit in suits:        
         values.extend([1,2,3,4,5,6,7,8,9,10,10,10,10,11]) #applys values to each entry
             
-    evaluation = dict(zip(deck, values)) #makes the dict
 
 
-        #caculates the winner and returns it dcard is dealer card p card is playercard, pretty easy to look out
-def Win_scenario(total_pcard, total_dcard):
-    if total_dcard > 21 and total_pcard <= 21:
+        #evaluates the given number/face and returns
 
-        print("Dealer Bust! {}, You Win!, {}".format(total_dcard, total_pcard))
-        win = True
+def evaluator(element):
+    match element[0]:
+        case '1':
+            return 1
+        case '2':
+            return 2
+        case '3':
+            return 3
+        case '4':
+            return 4
+        case '5':
+            return 5
+        case '6':
+            return 6
+        case '7':
+            return 7
+        case '8':
+            return 8
+        case '9':
+            return 9
+        case 'J':
+            return 10
+        case 'Q':
+            return 10
+        case 'K':
+            return 10
+        case 'A':
+            return 11
+        case _:
+            return 10
 
-    elif total_pcard > 21:
-
-        print("You Bust.. {}, Dealer Wins!, {}".format(total_pcard, total_dcard))
-        win = False
-
-    elif total_pcard > total_dcard:
-
-        print("You Win!, {}".format(total_pcard))
-        win = True
-
-    elif total_pcard == total_dcard:
-
-        print("Tie!")
-        win = None
-
-    else:
-        print("Dealer Wins, {}".format(total_dcard))
-        win = False
-
-    return win
-
-    #decided what to do based on the input of stand/hit
-def Optional(x):
-
-    if x in hlist:
-
-        print('You Hit')
-        card = hit()
-        player_cards.append(card) #adds the hit card to your hand and then decideds if the dealer should draw or not
-        print("You Drew: ", card)
-        if total_dcard <= 16:
-            card = hit()
-            dealer_cards.append(card)
-            print("Dealer Drew a Unkown Card")
-        return True
-
-    elif x in slist:
-
-        print('You Stand')
-        if total_dcard <= 16:
-            card = hit()
-            dealer_cards.append(card)
-            print("Dealer Drew a Unkown Card")
-
-        return False
-
-
-
-        #MAIN GAME FUNC
-def __Game__(money, stand, total_dcard, total_pcard, pot_money) -> None:
+def __Game__(money):
     
-    print("You have", total_pcard)
-    #while the player doesn't choose stand 
-    while stand:
-        
-            x = str(input("Would you like to Hit/Stand:\n"))
-            x = x.lower()
-            if x  in hlist or x in slist:
-                stand = bool(Optional(x))
-                
-                
-                player_cards_value = ([evaluation.get(pcards) for pcards in player_cards])
-                dealer_cards_value = ([evaluation.get(dcards) for dcards in dealer_cards])
-                
-                [int(elements) for elements in player_cards_value]
-                [int(elements) for elements in dealer_cards_value]
-                
-                for i in range(1):
-                    
-                    if stand:
-
-                        for element in range(2 + i, len(player_cards_value)):
-                            total_pcard += player_cards_value[element]
-                            if player_cards_value[element] == 11 and total_pcard > 21:
-                                    print('ace p')
-                                    total_pcard += -10
-                                    player_cards_value[element] = 1
-
-                        if total_pcard > 21:
-                            stand = False
-                        
-                    else:
-                        
-                        for element in range(2 + i, len(dealer_cards_value)):
-                            total_dcard += dealer_cards_value[element]
-
-                            if dealer_cards_value[element] == 11 and total_dcard > 21:
-                                    print('ace d')
-                                    total_pcard += -10
-                                    dealer_cards_value[element] = 1
-
-                        if total_dcard > 21:
-                            stand = False
-                        
-                    print("You have", total_pcard)
-                    i += 1
-
-                
-            else:
-                print("Invalid Option")
-                pause()
-                clear()
-                return __Game__(money, stand, total_dcard, total_pcard)
-
-    print("Your Cards Were {}".format(player_cards))
-    print("His Cards Were {}".format(dealer_cards))
-    win = Win_scenario(total_pcard, total_dcard)
     
-    if money != 0:
-        if win == True:
-            money += pot_money * 2
-            pot_money = 0
-        elif win == None:
-            money += pot_money
-            pot_money = 0
-        elif win == False:
-            pot_money = 0
-    elif pot_money != 0:
-        if win == True:
-            money += pot_money * 2
-            pot_money = 0
-        elif win == None:
-            money += pot_money
-            pot_money = 0
-        elif win == False:
-            pot_money = 0
-    else:
-        print("You Lost All Your Money :( Better Luck Next Time")
-        pause()
-    
-    if money != 0:
-        print("You have {}$ left".format(money))
-        x = input("Play Again? Y/N")
-        x = x.lower()
-        if x in ylist:
-            clear()
-            return __Initial__(money)
-        
-        else:
-            print("You Ended The Game With {}$".format(money))
-    else:
-        print("You Lost All Your Money :( Better Luck Next Time")
-        pause()
-    
-    breakf()
-def __Initial__(money) -> None:
-
-    global pot_money
-    stand = True
-
-    global total_dcard, total_pcard
-    total_pcard = int()
-    total_dcard = int()
-
-    global player_cards, dealer_cards
-    player_cards = []
-    dealer_cards = []
-    
-    global player_cards_value, dealer_cards_value
-    player_cards_value = []
-    dealer_cards_value = []
-
-    global ylist, hlist, slist
-    ylist = ('y', 'yes')
-    hlist = ('h', 'hit')
-    slist = ('s', 'stand')
-
     shuffle_deck()
     
+    #GAME VARIABLES
+    optionValues = ['s', 'stand', 'h', 'hit']
+    againValues = ['n', 'no', 'y', 'yes']
+    player_hand = []
+    dealer_hand = []
+    
+    player_hand_num = int()
+    dealer_hand_num = int()
 
-    print(ascii.black_jack)
-    pause()
-    clear()
-
-        #if someone inputs a invalid data type it returns an error and sends you back
-    try:
-        pot_money = int(input("Money to Put into Pot:\n"))
-    except:
-        print("Invalid Input")
+    player_hand_string = str()
+    dealer_hand_string = str()
+    
+    #Prompts for money No Cheating!
+    pot_money = int(input("Money to Put into Pot:\n"))
+    if pot_money < 0 or pot_money > money:
+        print("Cheaters Never Prosper")
         pause()
-        return __Initial__(money)
-
-        #if you try to enter an amount you dont have or otherwise it returns and error
-    if pot_money > money:
-            print("You dont have that Money!")
-            pause()
-            return __Initial__(money)
-    elif pot_money <= 0:
-            print("Invalid Amount")
-            pause()
-            return __Initial__(money)
-        
-        #tells you your money
+        raise Exception("Begone!")
     money -= pot_money
-    print("Money In Pot: {}\n Money In Balance: {}".format(pot_money, money))
+    
+    print("Your Balance: {}$\n Money in the Pot: {}$".format(money, pot_money))
     pause()
     clear()
     
-        #for 2 times the player and the dealer draws 2 cards
-    for _ in range(2):
-        card = hit()
-        player_cards.append(card)
-    for _ in range(2):
-        card = hit()
-        dealer_cards.append(card)
+    #deals 2 cards and increments 'i' by 1 to print the next item in list
+    i = 0
+    for i in range(2):
+        player_hand.append(hit())  
+        print("You Recived a", player_hand[i])
+        time.sleep(1)
+        dealer_hand.append(hit())
+        if i == 0:  
+            print("The Dealer Recived a", dealer_hand[0])
+        else:
+            print("The Dealer Recived an Unkown Card")
+        i += 1
+        time.sleep(1)
     
+    #for every card in your hand it is added to a numerical total that then is adjusted for aces
+    for element in player_hand:
+            element = evaluator(element)
+            player_hand_num += element
+            if player_hand_num > 21 and element == 11:
+                print("Fixed For Ace")
+                element = -10
+                player_hand_num += element
+               
+    print("You Have, ",player_hand_num)
     
-        #applies the dictionary to your cards into a new variable through a comprehension 
-    player_cards_value = ([evaluation.get(pcards) for pcards in player_cards])
-    dealer_cards_value = ([evaluation.get(dcards) for dcards in dealer_cards])
-
-    # converts to type int
-    [int(elements) for elements in player_cards_value]
-    [int(elements) for elements in dealer_cards_value]
-
-        #adds the total of the new list
-    for element in range(0, len(player_cards_value)):
-        total_pcard = total_pcard + player_cards_value[element]
-
-        if player_cards_value[element] == 11 and total_pcard > 21:
-                print('ace p')
-                total_pcard += -10
-                player_cards_value[element] = 1
-
-    for element in range(0, len(dealer_cards_value)):
-        total_dcard = total_dcard + dealer_cards_value[element]
-
-        if dealer_cards_value[element] == 11 and total_dcard > 21:
-                print('ace d')
-                total_pcard += -10
-                dealer_cards_value[element] = 1
+    for element in dealer_hand:
+            element = evaluator(element)
+            dealer_hand_num += element
+            if dealer_hand_num > 21 and element == 11:
+                element = -10
+                dealer_hand_num += element
+    
+    #repeats until both dealer and you stand or someone busts
+    
+    while True:
         
-        #tells what the dealer has and what you have
-
-    print("The Dealer Deals you a {0}".format(*player_cards))
-    pause()
-
-    print("To Himself a {0}".format(*dealer_cards))
-    pause()
-
-    print("Back to you a {1}".format(*player_cards))
-    pause()
-
-    print("Back to Himself a Unkown Card")
-    pause()
-
-    print("This Gives you a hand worth {} and him a hand worth at least {}".format(total_pcard, dealer_cards_value[0]))
-    pause()
-    clear()
-
-    clear()
-        #starts the rest of the game
-    __Game__(money, stand, total_dcard, total_pcard, pot_money)
-
-def __Setting__():
-    print(ascii.settings)
-    try:
-        money = int(input("Money To Start With: "))
-    except:
-        print("Invalid Option")
-        return __Setting__()
+        #prompts for user input to stand/hit the rest of the code is the same as above just looped
+            option = input("Stand or Hit:\n")
+            option = option.lower()
+            
+            if option in optionValues[:2]: #gets the first two values
+                
+                player_hand.append(hit())
+                element = player_hand[len(player_hand)-1]
+                
+                element = evaluator(element)
+                player_hand_num += element
+            
+                if player_hand_num > 21 and element == 11:
+                    element = -10
+                    player_hand_num += element
+                    
+                elif player_hand_num > 21:
+                    break
+                
+                
+                print("You Recived a", player_hand[i]) #i = card most recent
+                print("You Now Have, ",player_hand_num)
+                i += 1
+                if dealer_hand_num <= 16:
+                        
+                    dealer_hand.append(hit())
+                    element = dealer_hand[len(dealer_hand)-1]
+                        
+                    element = evaluator(element)
+                    dealer_hand_num += element
+                        
+                    if dealer_hand_num > 21 and element == 11:
+                        element = -10
+                        dealer_hand_num += element
+                    elif dealer_hand_num > 21:
+                        break
+                    
+                print("The Dealer Drew")
+                
+            elif dealer_hand_num <= 16:
+                        
+                dealer_hand.append(hit())
+                element = dealer_hand[len(dealer_hand)-1]
+                        
+                element = evaluator(element)
+                dealer_hand_num += element
+                        
+                if dealer_hand_num > 21 and element == 11:
+                    element = -10
+                    dealer_hand_num += element
+                elif dealer_hand_num > 21:
+                    break
+                
+                print("The Dealer Drew")
+            else:
+                break
+            
+        #makes a neat string value of all cards to be revealed at the end of the game
+    for i in player_hand:
+        player_hand_string += i+ ', '
+        
+    for i in dealer_hand:
+        dealer_hand_string += i+ ', '
+        
+        #the winning conditions simple math
+    if player_hand_num > 21:
+        print("You Lose, you had {} The dealer had {} You lost {}$".format(player_hand_string, dealer_hand_string, pot_money))
+        pot_money = 0
+        
+    elif dealer_hand_num > 21:
+        print("You Win, you had {} The dealer had {} You win {}$".format(player_hand_string, dealer_hand_string, pot_money))
+        money += pot_money + pot_money
+        
+    elif player_hand_num < dealer_hand_num:
+        print("You Lose, you had {} The dealer had {} You lost {}$".format(player_hand_string, dealer_hand_string, pot_money))
+        pot_money = 0
+        
+    else:
+        print("You Win, you had {} The dealer had {} You win {}$".format(player_hand_string, dealer_hand_string, pot_money))
+        money += pot_money + pot_money
+        
+        
+        #displays hand values
+    print(player_hand_num,' Vs. ',dealer_hand_num)
     
-    __Initial__(money)
     
+        #prompts to play again or if they dont have money then no entry
+    again = input("Play again? Y/N:\n")
+    again = again.lower()
+    
+    if again not in againValues[:2] and money > 0: #gets the first two items and make sure money is > 0
+        clear()
+        return __Game__(money)
+    
+    elif money == 0:
+        print("You have no money. Thanks For Playing!")
+        pause()
+        
+    else:
+        print("Thanks For Playing!")
+
+    breakf()
 if __name__ == "__main__":
     
     version = '1.0.0'
@@ -333,7 +260,14 @@ if __name__ == "__main__":
     print("Welcome To BlackJack, Made by Artyom Curtis Version {}".format(version))
     pause()
     clear()
-    __Setting__()
     
+    print(ascii.black_jack)
+    pause()
+    clear()
+    
+    money = 100
+    
+    print("You Start With 100$ you may want to try to increase it.")
+    __Game__(money)
             
     
